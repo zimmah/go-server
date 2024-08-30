8c4f4db4dd4df05f9525764d8bb04c0a89006bca
zimmah <zimmah@gmail.com>
2024-08-30 17:27:23 -0500

    fix file names

--- internal/router/respondWithJSON.go
+++ internal/router/respondWithJSON.go
@@ -1,18 +1,19 @@
 package router
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	if code > 499 {
-		log.Printf("Responding with 5XX error: %s", msg)
+func respondWithJSON(w http.ResponseWriter, statusCode int, payload any) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
-	type errorResponse struct {
-		Error string `json:"error"`
-	}
-	respondWithJSON(w, code, errorResponse{
-		Error: msg,
-	})
+	w.WriteHeader(statusCode)
+	w.Write(data)
 }
